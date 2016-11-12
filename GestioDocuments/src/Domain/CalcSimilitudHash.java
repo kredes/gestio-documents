0cd569031661a2afa1c033eb1437742916a505ac
AndresInsaurralde <andres.insaurralde@est.fib.upc.edu>
2016-11-12 13:51:29 +0100

    .

--- GestioDocuments/src/Domain/CalcSimilitudHash.java
+++
@@ -1,9 +0,0 @@
-package Domain;
-
-
-public class CalcSimilitudHash implements CalcSimilitud {
-    @Override
-    public double calculaSimilitud(Documento d1, Documento d2) {
-        return 0;
-    }
-}
