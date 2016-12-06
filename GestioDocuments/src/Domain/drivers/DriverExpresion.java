ced7d11e7d74c88ef46279760f30caf67995ae6b
alexsv96 <alex.serra.vidal@est.fib.upc.edu>
2016-12-06 14:00:09 +0100

    Collection para Expresion acabado

--- GestioDocuments/src/Domain/drivers/DriverExpresion.java
+++ GestioDocuments/src/Domain/drivers/DriverExpresion.java
@@ -12,7 +12,8 @@ public class DriverExpresion {
     public static void run() throws SyntaxErrorException, IOException {
         Expresion expresion = new Expresion();
         //ArrayList<Token> tokens = parser.generaTokens("{p1 p2 p3}   & (\"hola adios\" |pepe )  &     ! juan  ");
-        ArrayList<Token> tokens = expresion.generateTokens("{p1 p2 p3}&(\"hola adios\"|!((a|b)&c))&!juan");
+        //ArrayList<Token> tokens = expresion.generateTokens("{p1 p2 p3}&(\"hola adios\"|!((a|b)&c))&!juan");
+        ArrayList<Token> tokens = expresion.generateTokens("!sentencia & {patti dylan} & japonés");
 
         List<Token> postOrder = expresion.toPostOrder(tokens);
 
