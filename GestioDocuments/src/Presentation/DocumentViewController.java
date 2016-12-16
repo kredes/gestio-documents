4dc06509968f5f81aaea60f826e8bfda0bac7971
AndresInsaurralde <andres.insaurralde@est.fib.upc.edu>
2016-12-16 15:31:57 +0100

    .

--- GestioDocuments/src/Presentation/DocumentViewController.java
+++ GestioDocuments/src/Presentation/DocumentViewController.java
@@ -1,7 +1,18 @@
 package Presentation;
 
 
+import Domain.Documento;
+import Domain.DocumentoNoExiste;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class DocumentViewController extends ViewController {
+
+    @FXML private Label docViewTitulo;
+    @FXML private Label docViewAutores;
+    @FXML private Label docViewTag;
+    @FXML private TextArea docViewContenido;
 
     /* OVERRIDE */
     @Override
@@ -13,6 +24,15 @@ public class DocumentViewController extends ViewController {
     public void setUpView(String titulo, String autor) {
         super.setUpView();
 
+        try {
+            Documento doc = ctrlDominio.buscarDocumento(titulo, autor);
+            docViewTitulo.setText(doc.getTituloString());
+            docViewAutores.setText(String.join(",", doc.getAutoresStrings()));
+            docViewTag.setText(String.join(",", doc.getEtiquetasStrings()));
+            docViewContenido.setText(doc.getArticuloString());
+        } catch (DocumentoNoExiste e) {
+            // Lo que sea
+        }
     }
 
 }
